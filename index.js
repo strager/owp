67a8ce7766cad1cd343f4b1f69362e4333834794
strager <strager.nds@gmail.com>
2011-05-10 20:27:16 -0400

    Rewrite most of index to load better and be more organized overall

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
-/*global console: false, window: false */
-
 require([ 'jQuery', 'CanvasRenderer', 'RuleSet', 'Map', 'MapState', 'HitCircle', 'Skin', 'AssetManager', 'q' ], function ($, CanvasRenderer, RuleSet, Map, MapState, HitCircle, Skin, AssetManager, Q) {
-    function debug(message) {
-        console.log(message);
-    }
-
-    var skinPromise = (new AssetManager('.')).load('skin', 'skin');
-    var skinLoaded = false;
-
-    Q.when(skinPromise, function (skin) {
-        Q.when(skin.preload(), function () {
-            skinLoaded = true;
-        });
-    });
-
     var mapAssetManager = new AssetManager('assets');
+    var skinAssetManager = new AssetManager('.');
 
-    $(function () {
-        // Init
-        var mapLoaded = false;
+    var render = function (time, renderer, skin, mapInfo, mapState, mapAssetManager) {
+        renderer.beginRender();
+        renderer.renderStoryboard(mapInfo.storyboard, mapAssetManager, time);
+        renderer.renderMap(mapState, skin, time);
+        renderer.endRender();
+    };
 
-        var mapInfo = null;
-        var mapState = null;
-        var audio = null;
-
-        function getMapTime() {
-            return Math.round(audio.currentTime * 1000);
-        }
-
+    var init = function () {
         var canvas = document.createElement('canvas');
         canvas.width = 640;
         canvas.height = 480;
@@ -35,67 +17,98 @@ require([ 'jQuery', 'CanvasRenderer', 'RuleSet', 'Map', 'MapState', 'HitCircle',
 
         var canvasRenderer = new CanvasRenderer(canvas.getContext('2d'));
 
-        $(canvas).click(function (e) {
+        return {
+            renderer: canvasRenderer,
+            playArea: canvas
+        };
+    };
+
+    var preload = function () {
+        var audio;
+        var skin;
+        var mapState;
+        var mapInfo;
+
+        return Q.shallow([
+            Q.when(mapAssetManager.load('map', 'map'))
+                .then(function (mapInfo_) {
+                    mapInfo = mapInfo_;
+                    mapState = MapState.fromMapInfo(mapInfo);
+
+                    return Q.shallow([
+                        mapAssetManager.load(mapInfo.audioFile, 'audio'),
+                        mapInfo.storyboard.preload(mapAssetManager)
+                    ]);
+                })
+                .then(function (r) {
+                    audio = r[0];
+
+                    audio.controls = 'controls';
+                    document.body.appendChild(audio);
+
+                    return audio;
+                }),
+            Q.when(skinAssetManager.load('skin', 'skin'))
+                .then(function (skin_) {
+                    skin = skin_;
+
+                    return Q.when(skin_.preload());
+                })
+        ]).then(function () {
+            return {
+                audio: audio,
+                skin: skin,
+                mapState: mapState,
+                mapInfo: mapInfo
+            };
+        });
+    };
+
+    var game = function (io, gameInfo) {
+        var getMapTime = function () {
+            return Math.round(io.audio.currentTime * 1000);
+        };
+
+        var renderLoop = function () {
+            var time = getMapTime();
+
+            render(time, io.renderer, gameInfo.skin, gameInfo.mapInfo, gameInfo.mapState, mapAssetManager);
+
+            var renderInterval = 20;
+            setTimeout(renderLoop, renderInterval);
+        };
+
+        io.audio.currentTime = 33; // XXX TEMP
+        io.audio.play();
+
+        renderLoop();
+
+        $(io.playArea).click(function (e) {
             var x = e.pageX - this.offsetLeft;
             var y = e.pageY - this.offsetTop;
 
-            if (mapState) {
-                mapState.makeHit(x, y, getMapTime());
-            }
+            gameInfo.mapState.makeHit(x, y, getMapTime());
         });
+    };
 
-        // Render loop logic
-        var shouldRender = true;
+    $(function () {
+        Q.shallow([
+            init(),
+            preload()
+        ]).then(function (r) {
+            var io = {
+                renderer: r[0].renderer,
+                audio: r[1].audio,
+                playArea: r[0].playArea
+            };
 
-        function renderImpl() {
-            if (!skinLoaded || !mapLoaded) {
-                return;
-            }
+            var gameInfo = {
+                skin: r[1].skin,
+                mapState: r[1].mapState,
+                mapInfo: r[1].mapInfo
+            };
 
-            var time = getMapTime();
-
-            canvasRenderer.beginRender();
-
-            canvasRenderer.renderStoryboard(mapInfo.storyboard, mapAssetManager, time);
-
-            canvasRenderer.renderMap(mapState, skinPromise.valueOf(), time);
-
-            canvasRenderer.endRender();
-        }
-
-        function render() {
-            if (!shouldRender) {
-                return;
-            }
-
-            renderImpl();
-
-            var renderInterval = 20;
-            window.setTimeout(render, renderInterval);
-        }
-
-        // Start!
-        Q.when(mapAssetManager.load('map', 'map'), function (mapInfoParam) {
-            mapInfo = mapInfoParam;
-
-            mapState = MapState.fromMapInfo(mapInfo);
-
-            Q.shallow([
-                mapAssetManager.load(mapInfo.audioFile, 'audio'),
-                mapInfo.storyboard.preload(mapAssetManager)
-            ]).then(function (r) {
-                audio = r[0];
-
-                audio.controls = 'controls';
-                document.body.appendChild(audio);
-
-                audio.currentTime = 33; // XXX TEMP
-                audio.play();
-
-                mapLoaded = true;
-
-                render();
-            });
+            game(io, gameInfo);
         });
     });
 });
