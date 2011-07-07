c7e8257c52c485787215d101a5f057a04ad6e30f
Matt Glazar <matt.glazar@gmail.com>
2011-07-06 22:01:03 -0700

    Standards-conforming gPubSub

--- src/Util/gPubSub.js
+++ src/Util/gPubSub.js
@@ -4,22 +4,38 @@ define('Util/gPubSub', [ 'Util/PubSub' ], function (PubSub) {
     var events = new PubSub();
 
     window.addEventListener('message', function (event) {
-        if (event.data && event.data.secret === secret) {
-            events.publishSync.apply(
-                events,
-                event.data.args
-            );
-
-            event.preventDefault();
-            return false;
+        if (event.source !== window) {
+            return;
         }
+
+        var data;
+
+        try {
+            data = JSON.parse(event.data);
+        } catch (e) {
+            // Ignore non-JSON data
+            return;
+        }
+
+        if (data.secret !== secret) {
+            return;
+        }
+
+        events.publishSync.apply(
+            events,
+            event.data.args
+        );
+
+        event.preventDefault();
+        return false;
     }, false);
 
     return {
         publish: function () {
             var args = Array.prototype.slice.call(arguments, 1);
+            var data = JSON.stringify({ secret: secret, args: args });
 
-            window.postMessage({ secret: secret, args: args }, '*');
+            window.postMessage(data, '*');
         },
 
         subscribe: function (callback) {
