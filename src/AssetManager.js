330de47a84c9a0d76a6b7311f6a986648f881049
Matt Glazar <matt.glazar@gmail.com>
2011-08-06 21:12:14 -0700

    Change cleanup routine for Firefox to fix "can't click" issues

--- src/AssetManager.js
+++ src/AssetManager.js
@@ -66,6 +66,7 @@ define('AssetManager', [ 'MapInfo', 'mapFile', 'assetConfig', 'Util/Map', 'Util/
 
             function fail(event) {
                 if (audio.networkState === audio.NETWORK_NO_SOURCE) {
+                    cleanup();
                     ret.reject(new Error('NETWORK_NO_SOURCE'));
                 }
             }
@@ -81,10 +82,12 @@ define('AssetManager', [ 'MapInfo', 'mapFile', 'assetConfig', 'Util/Map', 'Util/
             vorbisTrack.onerror = fail;
 
             audio.addEventListener('canplaythrough', function () {
+                cleanup();
                 ret.resolve(audio);
             }, false);
 
             audio.addEventListener('error', function (event) {
+                cleanup();
                 ret.reject(new Error());
             }, false);
 
@@ -98,12 +101,14 @@ define('AssetManager', [ 'MapInfo', 'mapFile', 'assetConfig', 'Util/Map', 'Util/
             // to download all these pesky audio files.
             var globalName = randomGlobal();
             window[globalName] = audio;
-            function cleanup(x) {
+            function cleanup() {
                 delete window[globalName];
-                return x;
             }
 
-            return ret.promise.then(cleanup, cleanup);
+            // Note that we can't do ret.promise.then(..., ...) to execute
+            // cleanup() because of some weird Firefox quirk I can't be
+            // bothered to investigate.
+            return ret.promise;
         },
 
         sound: function (assetManager, name) {
