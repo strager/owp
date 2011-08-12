180bcddbf1770d5cb7fe7e450e5ab7900c9b9545
Matt Glazar <matt.glazar@gmail.com>
2011-08-11 22:34:12 -0700

    Use Unrequire.JS for module loading

--- owp.js
+++ owp.js
@@ -1,31 +1,14 @@
 DEBUG = true;
 
-(function () {
-    // Bullshit Require.js won't let us load scripts while *it* loads.
-    var head = document.getElementsByTagName('head')[0];
-    var script;
+// =D?
 
-    script = document.createElement('script');
-    script.async = true;
-    script.defer = true;
-    script.onload = function () {
-        script = document.createElement('script');
-        script.async = true;
-        script.defer = true;
-        script.onload = function () {
-            require({
-                baseUrl: 'src'
-            }, [ ], function () {
-                define('q', [ '../vendor/q/q' ], function (Q) {
-                    return Q;
-                });
-
-                require([ './index' ]);
-            });
-        };
-        script.src = 'vendor/require.js';
-        head.appendChild(script);
-    };
-    script.src = 'vendor/es5-shim.js';
-    head.appendChild(script);
-}());
+document.write('<script src="vendor/es5-shim.js"></script>');
+document.write('<script src="vendor/unrequirejs/lib/unrequire.js"></script>');
+document.write('<script>(' + function () {
+    require({
+        baseUrl: 'src',
+        aliases: {
+            'q': '../vendor/q/q'
+        }
+    }, [ '../index' ]);
+} + '())</script>');
