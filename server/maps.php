1981f293ca1683e7a35314271b3c4f35cd775980
Matt Glazar <matt.glazar@gmail.com>
2011-08-14 13:45:42 -0700

    Auto-read map from filesystem

--- server/maps.php
+++ server/maps.php
@@ -2,10 +2,33 @@
 
 require_once 'config.php';
 
-$maps = array(
-    'test' => array(
-        'text' => 'test',
-        'root' => 'assets',
-        'name' => 'map'
-    )
-);
+function getMaps($root) {
+    $maps = array();
+
+    $dirs = scandir($root);
+    foreach ($dirs as $dir) {
+        $mapDir = $root . '/' . $dir;
+        if (!is_numeric($dir) || !is_dir($mapDir)) {
+            continue;
+        }
+
+        $files = scandir($mapDir);
+
+        foreach($files as $file) {
+            $matches = null;
+            if (!preg_match('/^((.*) \((.*)\) \\[(.*)\\])\.osu$/', $file, $matches)) {
+                continue;
+            }
+
+            $maps[$dir . '.' . $matches[4]] = array(
+                'text' => $matches[2] . ' [' . $matches[4] . ']',
+                'name' => $matches[1],
+                'root' => $dir
+            );
+        }
+    }
+
+    return $maps;
+}
+
+$maps = getMaps('.');
