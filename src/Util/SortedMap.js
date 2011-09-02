08fffb05c3ec2987021b24062662b6dfdd283e7d
Matt Glazar <matt.glazar@gmail.com>
2011-09-02 00:04:26 -0700

    Move sorted map logic from History into SortedMap

---
+++ src/Util/SortedMap.js
@@ -0,0 +1,40 @@
+define('Util/SortedMap', [ ], function () {
+    function SortedMap() {
+        this.keys = [ ];
+        this.values = [ ];
+    }
+
+    // Returns the index in array whose value is greater than or equal to
+    // value
+    function sortIndex(array, value) {
+        var i;
+
+        // TODO Binary search
+        for (i = array.length; i --> 0; /* */) {
+            if (array[i] <= value) {
+                return i + 1;
+            }
+        }
+
+        return 0;
+    }
+
+    SortedMap.prototype = {
+        set: function (key, value) {
+            var index = sortIndex(this.keys, key);
+
+            if (this.keys[index] === key) {
+                this.values[index] = value;
+            } else {
+                this.keys.splice(index, 0, key);
+                this.values.splice(index, 0, value);
+            }
+        },
+
+        getIndexForKey: function (key) {
+            return sortIndex(this.keys, key);
+        }
+    };
+
+    return SortedMap;
+});
