88467ab15f15963cc2cb6be2ef8a257c21191678
Matt Glazar <matt.glazar@gmail.com>
2011-10-09 20:51:30 -0700

    Make playfield a class to fix fullscreen

--- server/src/templates/playfield.tpl.php
+++ server/src/templates/playfield.tpl.php
@@ -1,4 +1,4 @@
-<div id="<?php echo e($playfieldId); ?>">
+<div class="playfield" id="<?php echo e($playfieldId); ?>">
     <script type="text/javascript">document.write('<p>Loading...</p>');</script>
     <noscript>
         <p><strong style="color: red;">Error</strong>: JavaScript must be enabled to play.</p>
