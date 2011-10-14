3e667bbcf0420e331bcdfa51034d9e2bbdb54046
Matt Glazar <matt.glazar@gmail.com>
2011-10-13 22:07:00 -0700

    Style changes

--- server/src/templates/socialnetworks.tpl.php
+++ server/src/templates/socialnetworks.tpl.php
@@ -1,6 +1,6 @@
 <p class="social">
 <?php if ($twitter): ?>
-<a href="http://www.twitter.com/<?php e($twitter); ?>"><img src="http://twitter-badges.s3.amazonaws.com/t_logo-c.png" alt="Follow owp on Twitter"/></a>
+<a href="http://www.twitter.com/<?php e($twitter); ?>"><img src="http://twitter-badges.s3.amazonaws.com/t_logo-a.png" alt="Follow owp on Twitter"/></a>
 <?php endif; ?>
 </p>
 
