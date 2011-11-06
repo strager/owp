bdbcf38537062f575b00e2083bce305bb4873003
Matt Glazar <matt.glazar@gmail.com>
2011-11-05 18:30:02 -0700

    Fix latency issue by hacking around browser stubborness

--- src/Soundboard.js
+++ src/Soundboard.js
@@ -11,8 +11,41 @@ define('Soundboard', [ 'SoundPool' ], function (SoundPool) {
 
         getSoundPool: function (soundName) {
             if (!Object.prototype.hasOwnProperty.call(this.soundPools, soundName)) {
-                this.soundPools[soundName] = new SoundPool(this.assetManager.get(soundName, 'sound'));
-                this.soundPools[soundName].prealloc(3);
+                var pool = new SoundPool(this.assetManager.get(soundName, 'sound'));
+                pool.prealloc(3);
+                this.soundPools[soundName] = pool;
+
+                // Browsers have pretty bad latency problems on the first play.
+                // This hack works around that issue.
+                var sound = pool.alloc();
+                sound.volume = 0;
+
+                function played() {
+                    sound.removeEventListener('play', played, false);
+                    sound.removeEventListener('timeupdate', played, false);
+
+                    setTimeout(function () {
+                        function paused() {
+                            sound.removeEventListener('pause', paused, false);
+
+                            // Sound is now "hot" in the browser's cache (or
+                            // something), so latency is lower.  Still, we can't
+                            // set currentTime because browser are stubborn.  We
+                            // work around this with a hacky setTimeout...
+                            setTimeout(function () {
+                                sound.currentTime = 0;
+                                pool.free(sound);
+                            }, 1000);
+                        }
+
+                        sound.addEventListener('pause', paused, false);
+                        sound.pause();
+                    }, 500);
+                }
+
+                sound.addEventListener('play', played, false);
+                sound.addEventListener('timeupdate', played, false);
+                sound.play();
             }
 
             return this.soundPools[soundName];
